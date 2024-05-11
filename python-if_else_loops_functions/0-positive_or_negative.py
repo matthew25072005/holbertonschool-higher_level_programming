7792c15b3112edc155d3749c5be58efa6f0a773b
matthew25072005 <8855@holbertonstudents.com>
2024-05-11 23:59:56 +0000

    py

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0
+	print("is positive")
+
+	else if number < 0
+		print("is negative")
+
+	else number == 0
+		print("is zero")
