858e588f6aca56b61855c92e02760d1ec6baa514
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-17 14:46:08 +0000

    PY

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 def no_c(my_string):
     c = "c"
     c_mayus = "C"
-    new_string = ''.join ([i for i in my_string if i != c or c_mayus])
+    new_string = ''.join ([i for i in my_string if i != c and i != c_mayus])
     return new_string
