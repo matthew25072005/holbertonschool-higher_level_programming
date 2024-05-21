483d2e03365368a1fdd175cec2d9eab4354573f0
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-21 18:40:51 -0300

    Py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    """
+    Suma dos números enteros o flotantes y devuelve el resultado como un entero.
+
+    Args:
+        a (int or float): Primer número.
+        b (int or float, optional): Segundo número (por defecto es 98).
+
+    Returns:
+        int: Suma de a y b como un entero.
+    """
     if isinstance(a, int) and isinstance(b, int):
         return a + b
     elif isinstance(a, int) and isinstance(b, float):
