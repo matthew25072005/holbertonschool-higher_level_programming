745c741710197b589d2ca0c61b2c0706e82874fc
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-21 18:42:35 -0300

    PY

---
+++ python-test_driven_development/tests/0-add_integer.py
@@ -0,0 +1,10 @@
+    """
+    Suma dos números enteros o flotantes y devuelve el resultado como un entero.
+
+    Args:
+        a (int or float): Primer número.
+        b (int or float, optional): Segundo número (por defecto es 98).
+
+    Returns:
+        int: Suma de a y b como un entero.
+    """
