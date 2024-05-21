ab4c00165e270462596f2760e976dd71e3474140
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-21 19:02:51 -0300

    PY

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,5 +17,5 @@ def add_integer(a, b=98):
         raise TypeError("Ambos argumentos deben ser enteros o flotantes")
 
 # Ejemplo de uso
-resultado = add_integer (int(a), int(b))
+resultado = add_integer(1, 2)
 print(resultado)
