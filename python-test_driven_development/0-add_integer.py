556038cd85e40197c99ed2c8f893c54de8fd5b65
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-21 18:01:20 -0300

    Py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else if isinstance(a, int) and isinstance(b, float):
+        b = int(b)
+        return a + b
+    if isinstance(a, float) and isinstance(b, int):
+        a = int(a)
+        return a + b
+    else if isinstance(a, float) and isinstance(b, float):
+        a = int(a)
+        b = int(b)
+        return a + b
