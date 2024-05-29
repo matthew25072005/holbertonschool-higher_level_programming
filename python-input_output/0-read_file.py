4ff61ce1d2610ff10a1834f598ad68217b9e5d23
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-05-29 17:45:22 -0300

    PY

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""read_file.py: A function to read a text file (UTF8) and print it to stdout."""
 
 def read_file(filename=""):
     """Read a text file (UTF8) and print it to stdout.
@@ -10,8 +11,10 @@ def read_file(filename=""):
                      which will result in reading from standard input.
     """
     if filename:
+        # Open the file in read mode with UTF8 encoding
         with open(filename, 'r', encoding='utf8') as file:
+            # Read the file content and print it to stdout
             print(file.read())
     else:
+        # If no filename is provided, read from standard input
         print(input("Enter text to print: "))
-        
