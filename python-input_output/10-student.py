9380008b20b9c6a38342de3fc5da5505d850a16b
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-02 12:36:27 -0300

    PY

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """dictionary representation of a Student instance"""
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
