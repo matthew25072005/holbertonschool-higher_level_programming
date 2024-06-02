215c7ab8f1a12dc84ad6db50ea32ce5b3e31be89
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-02 12:00:46 -0300

    PY

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
-"""read_file.py: A function to read a text file (UTF8) and print it to stdout."""
+"""Define file reading function."""
+
 
 def read_file(filename=""):
-    """Read a text file (UTF8) and print it to stdout.
-
-    Uses the 'with' statement to open the file and ensures it is closed after
-    reading. Does not handle file permission or file does not exist exceptions.
-
-    :param filename: The name of the file to read. Defaults to an empty string,
-                     which will result in reading from standard input.
-    """
-    if filename:
-        # Open the file in read mode with UTF8 encoding
-        with open(filename, 'r', encoding='utf8') as file:
-            # Read the file content and print it to stdout
-            print(file.read())
-    else:
-        # If no filename is provided, read from standard input
-        print(input("Enter text to print: "))
+    """Print the content of read"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
