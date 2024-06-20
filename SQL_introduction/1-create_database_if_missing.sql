5b4698e071e12950bbea703cc93250d5e020e2bb
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-20 18:50:19 -0300

    SQL

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,13 +1,2 @@
-/**
- * Creates a new database named 'hbtn_0c_0'.
- * 
- * This SQL command is used to create a new database in a relational database management system.
- * 
- * Example:
- * To create a new database, simply execute this command in a SQL shell or client:
- * ```sql
- * CREATE DATABASE hbtn_0c_0;
- * ```
- * This will create a new database named 'hbtn_0c_0' if it does not already exist.
- */
+-- This SQL command is used to create a new database in a relational database management system.
 CREATE DATABASE hbtn_0c_0;
