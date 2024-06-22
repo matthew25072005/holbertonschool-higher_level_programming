bf55468548f5acbb95f560dd0b83aa2dfbc45142
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-22 19:26:38 -0300

    SQL

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- this script creates the database htn_0d_usa and the table states (in the database hbtn_0d_usa).
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states
+(
+    id INT UNIQUE NOT NULL
+    name VARCHAR(256)
+);
