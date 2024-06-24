a04dc241f6313d3ca60fdde62adf7cae2249b0f6
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-23 23:23:53 -0300

    SQL

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- this script list all genres of the show Dexter.
+SELECT tv_genres.name FROM tv_show_genres
+	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_shows.title = "Dexter"
+	ORDER BY name;
