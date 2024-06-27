87aa936ba611757abd03d28a73340d2dd052a8ad
matthew25072005 <159053234+matthew25072005@users.noreply.github.com>
2024-06-27 11:46:10 -0300

    PY

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-argStr = "{:d} argument"
-argc = len(sys.argv) - 1
-if argc == 0:
-    argStr += 's.'
-elif argc == 1:
-    argStr += ':'
-else:
-    argStr += 's:'
-print(argStr.format(argc))
-
-i = 0
-for arg in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, arg))
-    i += 1
+from sys import argv
+if __name__ == "__main__":
+    length = len(argv)
+    if length == 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(length - 1))
+        for i in range(length - 1):
+            print("{}: {}".format(i + 1, argv[i + 1]))
