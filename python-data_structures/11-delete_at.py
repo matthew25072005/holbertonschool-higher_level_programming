8e1179fac1bdabb6807e88d6c9a24b56f0c0d8d1
matthew25072005 <8855@holbertonstudents.com>
2024-07-25 09:35:39 -0300

    task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,12 @@
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    return my_list[:idx] + my_list[idx+1:]
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)  # Output: [1, 2, 3, 5]
+    print(my_list)   # Output: [1, 2, 3, 5]
