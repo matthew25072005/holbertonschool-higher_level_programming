45e3083f6c61775c8d4fe8b8233d45b37a538952
matthew25072005 <8855@holbertonstudents.com>
2024-07-27 10:13:24 -0300

    task 06

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,5 +6,10 @@ def print_sorted_dictionary(a_dictionary):
 
 # Example usage:
 if __name__ == "__main__":
-    a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+    a_dictionary = {
+        'language': "C",
+        'Number': 89,
+        'track': "Low level",
+        'ids': [1, 2, 3]
+    }
     print_sorted_dictionary(a_dictionary)
