cb76adf45f26b0392c4f84bdc9b17040855abd24
matthew25072005 <8855@holbertonstudents.com>
2024-08-04 23:02:49 -0300

    task 07

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
 
+# Importar las funciones desde sus respectivos módulos
+update_dictionary = __import__('7-update_dictionary').update_dictionary
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-# Example usage:
 if __name__ == "__main__":
-    update_dictionary = __import__('7-update_dictionary').update_dictionary
-    print_sorted_dictionary = 
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
     a_dictionary = {
         'language': "C",
         'number': 89,
         'track': "Low level"
     }
+    
+    # Actualizar el diccionario y mostrar el resultado
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
+    
     print("--")
+    
     print_sorted_dictionary(a_dictionary)
 
     print("--")
     print("--")
-
+    
+    # Añadir un nuevo elemento al diccionario y mostrar el resultado
     new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
     print_sorted_dictionary(new_dict)
+    
     print("--")
+    
     print_sorted_dictionary(a_dictionary)
