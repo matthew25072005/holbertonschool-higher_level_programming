8c322a0347aacec860177ae838988e9a76723d9a
matthew25072005 <8855@holbertonstudents.com>
2024-08-04 23:44:19 -0300

    task 01

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        # Intentar imprimir el valor usando el formato entero
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        # Captura ValueError y TypeError si el valor no puede ser formateado como entero
         return False
