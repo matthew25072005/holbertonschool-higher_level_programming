19c199f10ec2456f305aefb5702fbce9b3388a21
matthew25072005 <8855@holbertonstudents.com>
2024-08-04 23:14:41 -0300

    task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     # Verificar si el diccionario es None o está vacío
     if a_dictionary is None or not a_dictionary:
         return None
-    
+
     # Encontrar la clave con el valor máximo
     return max(a_dictionary, key=a_dictionary.get)
