bf3d6d647df3e1547920091945968140b41a5cd6
matthew25072005 <8855@holbertonstudents.com>
2024-08-04 23:56:26 -0300

    task 04

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            # Intenta obtener los elementos de las listas
+            elem1 = my_list_1[i]
+            elem2 = my_list_2[i]
+            
+            # Intenta realizar la división
+            try:
+                result.append(elem1 / elem2)
+            except ZeroDivisionError:
+                print("division by 0")
+                result.append(0)
+            except TypeError:
+                print("wrong type")
+                result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except Exception as e:
+            print("An unexpected error occurred: {}".format(e))
+            result.append(0)
+    
+    return result
