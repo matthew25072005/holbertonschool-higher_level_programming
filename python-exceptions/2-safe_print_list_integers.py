b69fc7444ebe8cdfc3a97c4a8cf6b9891a3c2976
matthew25072005 <8855@holbertonstudents.com>
2024-08-04 23:41:11 -0300

    task02

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end=' ')
+                print("{:d}".format(my_list[i]), end='')
                 count += 1
-    except IndexError:
-        # Si ocurre IndexError, significa que hemos llegado al final de la lista
-        pass
-    print()  # Para finalizar la línea después de imprimir todos los enteros
+        except IndexError:
+            break
+    print()
     return count
