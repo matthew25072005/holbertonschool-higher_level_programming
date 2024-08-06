1dcede5729d87ee3c7aab41ea2541b55f28280c5
matthew25072005 <8855@holbertonstudents.com>
2024-08-06 15:37:04 -0300

    task 01

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
 
 class Square:
+    """Represent a square"""
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
