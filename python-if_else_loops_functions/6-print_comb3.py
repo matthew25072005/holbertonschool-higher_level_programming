4d17af82b1775da6bcdad648a12c2ea553965c6f
matthew25072005 <8855@holbertonstudents.com>
2024-09-05 18:37:11 -0300

    task 06

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,10 @@ for i in range(9):
     f_n = i
     i += 1
     num = str(f_n) + str(i)
-    print("{}, ".format(num), end="")
+    if int(num) == 89:
+        print("{}\n".format(num), end="")
+    else:
+        print("{}, ".format(num), end="")
     while i != 9:
         i += 1
         print("{}{}, ".format(f_n, i), end="")
