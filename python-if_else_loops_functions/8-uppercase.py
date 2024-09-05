7644b0dcfa1034eaa93d8a6ad043a0bf0012bd3f
matthew25072005 <8855@holbertonstudents.com>
2024-09-05 19:55:47 -0300

    task 08

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def uppercase(s):
-    print("{}".format(s.upper()))
+def uppercase(c):
+    result = ""
+    for char in c:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    
+    print("{}".format(result))
