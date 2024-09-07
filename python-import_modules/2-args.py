55b0739c13a294521c21f6506525bbb49c9ea8e4
matthew25072005 <8855@holbertonstudents.com>
2024-09-07 16:38:01 -0300

    task 02

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if num_args == 0:
         print("Number of arguments: 0.")
     elif num_args == 1:
-        print("Number of argument: 1:")
+        print("Number of argument 1:")
     else:
         print(f"Number of arguments: {num_args}:")
 
