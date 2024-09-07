e413c79aed7898fea89037471af7faae0645d02f
matthew25072005 <8855@holbertonstudents.com>
2024-09-07 16:32:21 -0300

    task 02

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("Number of arguments: 0.")
+    elif num_args == 1:
+        print("Number of argument: 1:")
+    else:
+        print(f"Number of arguments: {num_args}:")
+
+    for i in range(1, len(sys.argv)):
+        print(f"{i}: {sys.argv[i]}")
