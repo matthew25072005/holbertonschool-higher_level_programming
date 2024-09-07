4fec57e0b04d638ddb112452eb6243dcef6d8800
matthew25072005 <8855@holbertonstudents.com>
2024-09-06 21:21:00 -0300

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def fizzbuzz():
+    i = 0
+    while i < 100:
+        i += 1
+        if i % 3 == 0:
+            print("Fizz ")
+        elif i % 5 == 0:
+            print("Buzz ")
+        else:
+            print("{} ".format(i))
