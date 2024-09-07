e8759974dbb858a30312e7b677332d3d5dbad5cf
matthew25072005 <8855@holbertonstudents.com>
2024-09-07 18:53:28 -0300

    task 05

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-import importlib.util
-
-def main():
-    file_path = '/tmp/variable_load_5.py'
-    spec = importlib.util.spec_from_file_location("variable_load_5", file_path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    print(module.a)
-
-if __name__ == '__main__':
-    main()
+from variable_load_5 import a
+if (__name__ == "__main__"):
+    print(a)
