4dd712e9d7f1faecaf4e2523635cc3379dbec558
matthew25072005 <8855@holbertonstudents.com>
2024-09-12 11:24:09 -0300

    task 07

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,4 +4,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b = tuple_b + (0, 0)
     suma1 = tuple_a[0] + tuple_b[0]
     suma2 = tuple_a[1] + tuple_b[1]
-    print ("({}, {})".format(suma1, suma2), end="")
+    return "({}, {})".format(suma1, suma2)
