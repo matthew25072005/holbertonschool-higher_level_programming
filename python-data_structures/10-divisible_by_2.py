62f08f9387eea5dfb8bc93309618c02544b424ea
matthew25072005 <8855@holbertonstudents.com>
2024-09-12 15:34:08 -0300

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,10 +3,12 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return None
 
-    num = my_list[0]
     for i in my_list:
         if i % 2 == 0:
-            num = i
-            print("{} is divisible by 2".format(num))
+            list_result.append(True)
+            print("{} is divisible by 2".format(i))
         else:
-            print("{} is not divisible by 2".format(num))
+            list_result.append(False)
+            print("{} is not divisible by 2".format(i))
+
+    return list_result 
