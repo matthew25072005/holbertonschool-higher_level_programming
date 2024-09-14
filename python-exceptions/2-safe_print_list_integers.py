a2a152060dc6113fd7bc5d8cb4a377d8f01f8c03
matthew25072005 <8855@holbertonstudents.com>
2024-09-14 18:35:29 -0300

    task 02

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    i = 0
-    for index in range(x):
-        try:
-            print("{:d}".format(my_list[index]), end="")  # Intenta imprimir como entero
-            i += 1
-        except (ValueError, TypeError):
-            continue
-        except IndexError:
-            break
-    print("")
-    return i
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            printed_count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return printed_count
