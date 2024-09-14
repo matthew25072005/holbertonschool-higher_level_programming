e99fb1ea31a5c18eca9ab0d0ed6162ac8309b531
matthew25072005 <8855@holbertonstudents.com>
2024-09-14 16:27:30 -0300

    task 01

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))  # Intento de imprimir como entero
+        return True
+    except (ValueError, TypeError):  # Captura si no es posible formatear como entero
+        return False
