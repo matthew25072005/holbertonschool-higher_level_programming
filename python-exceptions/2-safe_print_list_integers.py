135ec1f6793f1543ccdb1081c0672ad70cfe6bcf
matthew25072005 <8855@holbertonstudents.com>
2024-09-14 18:39:04 -0300

    task 02

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
         except IndexError:
+            print("Traceback (most recent call last):", file=sys.stderr)
             break
         i += 1
     print()
