5fdae8b56ba05aa7776e951ece1472c4ad3ef407
matthew25072005 <8855@holbertonstudents.com>
2024-09-18 10:26:10 -0300

    task 01

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+
 class Square:
+    """
+    Esta clase representa un cuadrado.
+
+    Atributos:
+        __s (int): Tamaño del lado del cuadrado.
+    """
+
     def __init__(self, s):
-        self.__s = size
+        """
+        Inicializa una instancia de la clase Square.
+
+        Parámetros:
+            s (int): Tamaño del lado del cuadrado.
+
+        Nota:
+            El tamaño del lado se almacena en un atributo privado __s.
+        """
+        self.__s = s
