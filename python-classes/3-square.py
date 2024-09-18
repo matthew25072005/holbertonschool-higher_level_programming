6585c48f903d426fd2700b92d4adef8563cd37b7
matthew25072005 <8855@holbertonstudents.com>
2024-09-18 11:29:15 -0300

    task 03

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -26,6 +26,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """
         Calcula y retorna el área del cuadrado.
