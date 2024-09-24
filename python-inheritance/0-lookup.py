cc3d387a19c570fed374898fe901867373662719
matthew25072005 <8855@holbertonstudents.com>
2024-09-24 01:07:37 -0300

    task 00

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines a Rectangle"""
+
 
 def lookup(obj):
+    """
+    Devuelve la lista de atributos y métodos disponibles de un objeto.
+
+    Parámetros:
+        obj: El objeto del cual obtener la lista de atributos y métodos.
+
+    Retorna:
+        list: Lista de atributos y métodos del objeto.
+    """
     return dir(obj)
