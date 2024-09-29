cdc28edb05e4cfbe608d0ede97c3bc348cf1cfa6
matthew25072005 <8855@holbertonstudents.com>
2024-09-29 19:38:13 -0300

    taask 08

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -43,3 +43,6 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+if __name__ == "__main__":
+    print(issubclass(Rectangle, BaseGeometry))  # This should print True
