5ea19222e6fcd6fdd52855eea40d78c79ef90a2b
matthew25072005 <8855@holbertonstudents.com>
2024-09-29 19:44:34 -0300

    task 08

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,49 +1,19 @@
 #!/usr/bin/python3
 """
-Module 8-rectangle
-This module contains the definition of a Rectangle class that inherits from
-BaseGeometry. The Rectangle class is used to represent rectangles with width
-and height, both of which are validated to be positive integers.
+Class BaseGeometry
+Improved Geometry with Rectangle class
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""superclass import """
 
-class BaseGeometry:
-    """Base class for geometry."""
-    
-    def area(self):
-        """Raises an Exception with the message area() is not implemented."""
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """Validates that value is a positive integer.
-        
-        Args:
-            name (str): The name of the parameter.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
 
 class Rectangle(BaseGeometry):
-    """Class that represents a rectangle."""
-    
+    """
+    Rectangle inheritated of BaseGometry got width and height
+    """
+
     def __init__(self, width, height):
-        """Initialize the rectangle with width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
-        self.integer_validator("height", height)
         self.__height = height
-
-# Test to check if Rectangle is a subclass of BaseGeometry
-if __name__ == "__main__":
-    print(issubclass(Rectangle, BaseGeometry))  # This should print True
