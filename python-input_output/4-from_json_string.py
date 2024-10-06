c0aba4f6fa9708e9b40e1ff5ee728d24bfa22c90
matthew25072005 <8855@holbertonstudents.com>
2024-10-06 08:55:10 -0300

    task 04

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
-
-Este módulo contiene una función para convertir una cadena JSON en una 
+Este módulo contiene una función para convertir una cadena JSON en una
 estructura de datos de Python.
 
 
-El módulo utiliza el paquete estándar `json` de Python para realizar la 
+El módulo utiliza el paquete estándar `json` de Python para realizar la
 deserialización, lo que convierte una cadena JSON en un objeto nativo de
-Python, 
+Python,
 como un diccionario o una lista.
 
 
 Funciones:
-    - from_json_string(my_str): Convierte una cadena JSON en un objeto de 
+    - from_json_string(my_str): Convierte una cadena JSON en un objeto de
       Python.
 
 
 Uso:
-    Este módulo permite convertir una cadena en formato JSON en un objeto 
-    nativo de Python que puede ser manipulado. No maneja excepciones en caso 
+    Este módulo permite convertir una cadena en formato JSON en un objeto
+    nativo de Python que puede ser manipulado. No maneja excepciones en caso
     de que la cadena JSON no represente un objeto válido.
 """
 
