10d2556fc09828fb40e594aaf0f3762ffe5cceba
matthew25072005 <8855@holbertonstudents.com>
2024-10-06 08:10:53 -0300

    task 03

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""
-Este módulo está diseñado para ser ejecutado con Python 3.
+    """
+    Este módulo está diseñado para ser ejecutado con Python 3.
 
-El uso de la línea `#!/usr/bin/python3` al inicio del archivo es conocido
-como *shebang*. Esto indica al sistema operativo que el script debe ser
-ejecutado utilizando el intérprete de Python 3.
+    El uso de la línea `#!/usr/bin/python3` al inicio del archivo es conocido
+    como *shebang*. Esto indica al sistema operativo que el script debe ser
+    ejecutado utilizando el intérprete de Python 3.
 
-Estructura:
-    Este módulo aún no tiene funcionalidades implementadas, pero puede ser
-    utilizado para escribir código y ejecutarlo directamente en un entorno
-    compatible con Python 3.
+    Estructura:
+        Este módulo aún no tiene funcionalidades implementadas, pero puede ser
+        utilizado para escribir código y ejecutarlo directamente en un entorno
+        compatible con Python 3.
 
-Funciones:
-    No se han definido funciones todavía.
+    Funciones:
+        No se han definido funciones todavía.
 
-Clases:
-    No se han definido clases todavía.
+    Clases:
+        No se han definido clases todavía.
 
-Ejemplo de uso:
-    Para ejecutar este módulo en un entorno Unix/Linux, puedes asegurarte de
-    que el archivo sea ejecutable utilizando el siguiente comando:
+    Ejemplo de uso:
+        Para ejecutar este módulo en un entorno Unix/Linux, puedes asegurarte de
+        que el archivo sea ejecutable utilizando el siguiente comando:
 
-    $ chmod +x nombre_del_archivo.py
-    $ ./nombre_del_archivo.py
-"""
+        $ chmod +x nombre_del_archivo.py
+        $ ./nombre_del_archivo.py
+    """
 
 # Aquí es donde se implementará el código del módulo.
 
