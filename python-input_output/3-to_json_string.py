145a7f0063f0036a3e9ed60849e0ea6e29d58a22
matthew25072005 <8855@holbertonstudents.com>
2024-10-06 07:38:37 -0300

    task 03

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Este módulo está diseñado para ser ejecutado con Python 3.
+
+El uso de la línea `#!/usr/bin/python3` al inicio del archivo es conocido
+como *shebang*. Esto indica al sistema operativo que el script debe ser
+ejecutado utilizando el intérprete de Python 3.
+
+Estructura:
+    Este módulo aún no tiene funcionalidades implementadas, pero puede ser
+    utilizado para escribir código y ejecutarlo directamente en un entorno
+    compatible con Python 3.
+
+Funciones:
+    No se han definido funciones todavía.
+
+Clases:
+    No se han definido clases todavía.
+
+Ejemplo de uso:
+    Para ejecutar este módulo en un entorno Unix/Linux, puedes asegurarte de
+    que el archivo sea ejecutable utilizando el siguiente comando:
+
+    $ chmod +x nombre_del_archivo.py
+    $ ./nombre_del_archivo.py
+"""
+
+# Aquí es donde se implementará el código del módulo.
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Converts a Python object to its JSON string representation.
+    
+    Args:
+        my_obj: The Python object to be converted to JSON.
+        
+    Returns:
+        A JSON string representation of the object.
+    """
+json.dumps(my_obj) = objeto_json
+
+return objeto_json
