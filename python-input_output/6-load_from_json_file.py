5183e86bac2bb8c9f194e0154845bd070766f168
matthew25072005 <8855@holbertonstudents.com>
2024-10-06 21:12:14 -0300

    task 07

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,9 +7,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """
-    function that creates an object from a "JSON file"
-    """
+    """Creates an object from a JSON file."""
     with open(filename, 'r') as f:
-        r_f = json.load(f)
-        return r_f
+        return json.load(f)
