e8787870c61a2b00e77ddc73652fcb154883b59d
matthew25072005 <8855@holbertonstudents.com>
2024-10-16 09:54:34 -0300

    task 02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
 
 import requests
+import csv
 
 def fetch_and_print_posts():
     """
-    look for posts from JSONPlaceholder and prints the status code and post titles.
+    Fetch posts from JSONPlaceholder and print the status code and titles.
     """
-    info = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print (info.status_code)
-    if info.status_code == 200:
-        datos = info.json()
-        for post in datos:
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        
+        for post in posts:
             print(post['title'])
 
-def save_data():
-    info = requests.get("https://jsonplaceholder.typicode.com/posts")
+def fetch_and_save_posts():
+    """
+    Fetch posts from JSONPlaceholder and save them in a CSV file.
+    """
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        
+        data_to_save = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        
+        with open('posts.csv', mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            
+            writer.writeheader()
+            
+            writer.writerows(data_to_save)
+
+fetch_and_print_posts()
+fetch_and_save_posts()
