043a13adbc9c316739da69dbdc859766dcaaa069
matthew25072005 <8855@holbertonstudents.com>
2024-10-16 13:04:51 -0300

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -36,7 +36,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            error_message = {"Endpoint not found"}
+            error_message = {"error": "404 Not Found"}
             self.wfile.write(json.dumps(error_message).encode())
 
 if __name__ == "__main__":
