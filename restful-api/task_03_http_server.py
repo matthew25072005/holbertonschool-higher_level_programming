6bd90c49dc23eaefcab2cffd47d30d16720ff847
matthew25072005 <8855@holbertonstudents.com>
2024-10-16 10:19:53 -0300

    task 03

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class mmg(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello, this is a simple API!")
