82b8b513881a251babdfdd2b04cc555826c381a0
matthew25072005 <8855@holbertonstudents.com>
2024-10-16 13:01:38 -0300

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -33,10 +33,10 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         else:
             # Maneja cualquier otra ruta no definida
-            self.send_response(404 Not Found)
+            self.send_response(404)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
+            error_message = {"Endpoint not found"}
             self.wfile.write(json.dumps(error_message).encode())
 
 if __name__ == "__main__":
