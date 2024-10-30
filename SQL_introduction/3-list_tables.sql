986ac1d3198e623bd645f23bcd5ffa30e734168b
matthew25072005 <8855@holbertonstudents.com>
2024-10-30 15:07:29 -0300

    task 03

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
--- Lists all tables from the specified database
-SELECT table_name 
-FROM information_schema.tables 
+-- List all tables from the specified database without extra headers
+SELECT table_name AS 'Tables_in_hbtn_test_db_0'
+FROM information_schema.tables
 WHERE table_schema = DATABASE();
