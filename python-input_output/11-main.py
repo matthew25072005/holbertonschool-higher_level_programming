e9e2300bbe72d686df847b545bbd3ff51468e140
matthew25072005 <8855@holbertonstudents.com>
2024-11-14 20:43:57 -0300

    task 11

--- python-input_output/11-main.py
+++ python-input_output/11-main.py
@@ -1,42 +1,26 @@
 #!/usr/bin/python3
-import os
-import sys
-
-Student = __import__('11-student').Student
-read_file = __import__('0-read_file').read_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-path = sys.argv[1]
-
-if os.path.exists(path):
-    os.remove(path)
-
-student_1 = Student("John", "Doe", 23)
-j_student_1 = student_1.to_json()
-print("Initial student:")
-print(student_1)
-print(type(student_1))
-print(type(j_student_1))
-print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+"""Python executable file."""
 
 
-save_to_json_file(j_student_1, path)
-read_file(path)
-print("\nSaved to disk")
+class Student:
+    """Student class creation."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
+    def to_json(self, attrs=None):
+        """Function that converts instance into JSON format."""
+        result = {}
+        if attrs is not None:
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+        else:
+            for key, value in self.__dict__.items():
+                result[key] = value
+        return result
 
-print("Fake student:")
-new_student_1 = Student("Fake", "Fake", 89)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
-
-
-print("Load dictionary from file:")
-new_j_student_1 = load_from_json_file(path)
-
-new_student_1.reload_from_json(j_student_1)
-print(new_student_1)
-print(type(new_student_1))
-print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
