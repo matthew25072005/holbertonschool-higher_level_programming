fa275e1776cfdf6f9db5de1f8649d0e6e4d63794
matthew25072005 <8855@holbertonstudents.com>
2024-11-14 21:01:50 -0300

    task 07

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,9 @@
--- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-INSERT INTO first_table (
-    id 89,
-    name Best School
+-- script that inserts a new row in the table in your MySQL server.
+INSERT INTO first_table(
+    id,
+    name
+)
+VALUES(
+    89,
+    "Best School"
 );
