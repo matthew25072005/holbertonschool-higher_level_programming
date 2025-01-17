d382a6a1e88e9f36a9a9f40ea9e01c83f3b8d880
matthew25072005 <8855@holbertonstudents.com>
2025-01-17 17:11:54 -0300

    task 05

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 i = 0
-while i >= 0 and i < 99:
+while i >= 0 and i < 10:
+    char = str(i)
+    print(str(0) + str.format(char), end=", ")
+    i += 1
+while i >= 10 and i < 99:
     char = str(i)
     print(str.format(char), end=", ")
     i += 1
