12af76cdb186a7d871e8799d15aac00f7591fac4
matthew25072005 <8855@holbertonstudents.com>
2025-01-19 18:31:57 -0300

    task 06

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,9 +5,9 @@ i = 10
 
 while n2 <= i and n1 < 9:
     if n1 == 8:
-        print(f"{n1}{n2}")
+        print(f"{str.format(n1)}{str.format(n2)}")
     else:
-        print(f"{n1}{n2}", end=", ")
+        print(f"{str.format(n1)}{str.format(n2)}", end=", ")
     n2 += 1
     if n2 == i and n1 < 9:
         n1 += 1
