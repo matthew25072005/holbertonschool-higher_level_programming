dec1c25c0835de56709388cc6b13c3b18800beec
matthew25072005 <8855@holbertonstudents.com>
2025-01-19 23:59:28 -0300

    task 5

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def main():
+    """Import the variable `a` from `variable_load_5.py` and print its value."""
+    from variable_load_5 import a
+    print(a)
+
+if __name__ == "__main__":
+    main()
