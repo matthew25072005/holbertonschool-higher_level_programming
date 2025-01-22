5eb979bc4717f2f14e1f64dc96f37bbcc02099d0
matthew25072005 <8855@holbertonstudents.com>
2025-01-22 14:02:28 -0300

    task 01

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    elemento = my_list[idx]
-    print(f"Element at index {idx} is {elemento}")
+    largo = len(my_list)
+    if idx > 0 and idx < largo:
+        elemento = my_list[idx]
+        print(f"Element at index {idx} is {elemento}")
+    else:
+        print(f"Element at index {idx} is None")
