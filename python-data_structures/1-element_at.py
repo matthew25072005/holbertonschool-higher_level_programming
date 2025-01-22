c11dd2f72c7488bf5fe366c6c5a0b06b7b4e535d
matthew25072005 <8855@holbertonstudents.com>
2025-01-22 13:21:45 -0300

    task 01

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx > 0 and my_list.index(idx):
-        print("Element at index {:d} is {:d}".format(idx, element_at(list, idx)))
+    elemento = my_list.index(idx)
+    print("Element at index {:d} is {:d}".format(idx, elemento))
