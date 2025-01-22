33a0dde8b5cf8b834a18cefc7b1687bbef5d2c75
matthew25072005 <8855@holbertonstudents.com>
2025-01-22 11:44:52 -0300

    task 00

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5]
     i = 0
-    while i < 5:
-        valor == my_list(i)
-        print (str.format(valor))
-        i += 1
+    print (str.format(my_list[i]))
+    i += 1
