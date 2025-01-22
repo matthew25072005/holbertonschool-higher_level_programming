16201cccaf36126b8b3b7127a6876e78af57581e
matthew25072005 <8855@holbertonstudents.com>
2025-01-22 11:57:06 -0300

    task 00

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in my_list:
-        char = str(my_list)
+        char = str.format(my_list)
         print(str.format(char[i]), end="\n")
