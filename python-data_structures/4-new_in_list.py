150e0d6e4f3a8d11e4481e03b88e5fc816b38d4c
matthew25072005 <8855@holbertonstudents.com>
2025-01-23 17:43:36 -0300

    task 04

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list1 = list(my_list)
+    largo = len(list1)
+    if idx >= 0 and idx < largo:
+        list1[idx] = element
+        return list1
