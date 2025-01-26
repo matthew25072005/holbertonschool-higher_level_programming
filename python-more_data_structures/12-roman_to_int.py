7e22cd587cdfa538f5dca8c709a0c4746e114b93
matthew25072005 <8855@holbertonstudents.com>
2025-01-26 20:10:51 -0300

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
 
