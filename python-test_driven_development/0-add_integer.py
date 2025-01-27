90b1f2e8466f29ec39333d4cdf776ed72b4d12f6
matthew25072005 <8855@holbertonstudents.com>
2025-01-26 22:02:42 -0300

    task 00

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""Add integer function."""
+import math
 
 def add_integer(a, b=98):
     """ 
     Returns the sum of 'a' and 'b'.
-
+    
     Args:
         a (int): first integer to be added to sum.
         b (int): second integer to be added to sum.
-
+    
     Returns:
         int: The sum of a + b
   
     Raises:
         TypeError: "{a or b} must be an integer"
     """
-
-    # Convert both a and b to integers if they are floats
+    
+    if isinstance(a, float) and math.isnan(a):
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and math.isnan(b):
+        raise TypeError("b must be an integer")
+    
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-
+    
     return int(a) + int(b)
