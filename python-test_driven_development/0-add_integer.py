9ba3b64dd0ba69ec8d90b046ff047089c4403508
matthew25072005 <8855@holbertonstudents.com>
2025-01-26 22:14:36 -0300

    task 00

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import math
+"""Add integer function."""
 
 def add_integer(a, b=98):
     """ 
@@ -11,19 +11,19 @@ def add_integer(a, b=98):
     
     Returns:
         int: The sum of a + b
-  
     Raises:
         TypeError: "{a or b} must be an integer"
     """
     
-    if isinstance(a, float) and math.isnan(a):
+    # Check for NaN by using the fact that NaN is not equal to itself
+    if a != a:
         raise TypeError("a must be an integer")
-    if isinstance(b, float) and math.isnan(b):
+    if b != b:
         raise TypeError("b must be an integer")
     
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    
+
     return int(a) + int(b)
