c0e9b26a5804209bc3d8fc1b186d76a660d77c69
matthew25072005 <8855@holbertonstudents.com>
2025-01-26 21:29:04 -0300

    task 00

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@ Function that adds two integers (or floats) and returns an integer.
 """
 
 def add_integer(a, b=98):
+    """Adds two integers or floats."""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
