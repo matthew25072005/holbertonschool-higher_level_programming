c2b69036ac23b0ebaa511643a6774b09eaffe9b9
matthew25072005 <8855@holbertonstudents.com>
2025-01-29 12:59:37 -0300

    task 03

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Square.
+"""
+
+
+class Square:
+    """
+    Represents a square.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    def size(self, value):
+        
