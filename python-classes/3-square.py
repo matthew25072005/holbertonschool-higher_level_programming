8bbc87d09c30bc2f10632a80567e73f38b91c2bc
matthew25072005 <8855@holbertonstudents.com>
2025-01-29 11:23:17 -0300

    task 03

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,13 +8,13 @@ class Square:
     """
     Represents a square.
     """
-    def __init__(self, size=0, area):
+    def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        def area(self, size=0):
-            self.__size = size
-            area = size * size
-            return size
+    def __init__(self, size=0, area):
+        self.__size = size
+        self.area = size * size
+        return size
