fb0e20794e046fd181c824ee039d405d4998ce18
matthew25072005 <8855@holbertonstudents.com>
2025-02-02 20:07:17 -0300

    task 01

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,17 +11,16 @@ class Rectangle:
 
 
     def __init__(self, width=0, height=0):
-
+        self.__width = width
+        self.__height = height
 
     def width(self, value):
-        self.__width = width
         if width is not int:
             raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be >= 0")
         
     def height(self, value):
-        self.__height = height
         if height is not int:
             raise TypeError("height must be an integer")
         elif height < 0:
