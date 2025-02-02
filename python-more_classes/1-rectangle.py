06e5c4440a3f13b3347084fc292eda50f260e840
matthew25072005 <8855@holbertonstudents.com>
2025-02-02 20:00:45 -0300

    task 01

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+    """
+
+    def width(self, value):
+        self.__width = width
+        if width is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        
+    def height(self, value):
+        self.__height = height
+        if height is not int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+    
+    def __init__(self, width=0, height=0):
