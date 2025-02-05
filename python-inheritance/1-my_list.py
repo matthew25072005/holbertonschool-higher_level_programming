4c9c7f59314a90f632070ff1618e682e909e5edf
matthew25072005 <8855@holbertonstudents.com>
2025-02-05 14:50:26 -0300

    task 01

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a class named lookup.
+"""
+
 
 class MyList:
     def print_sorted(self):
+    """
+    this fintion print a sorted list
+    """
         print(sort(self))
-    
