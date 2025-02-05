282427865310b8ce29ce436d400a9532dabbc28b
matthew25072005 <8855@holbertonstudents.com>
2025-02-05 13:43:22 -0300

    task 00

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a class named lookup.
+"""
 
 def lookup(obj):
     """
-        this fintion return a list
+    this fintion return a list
     """
     return dir(obj)
