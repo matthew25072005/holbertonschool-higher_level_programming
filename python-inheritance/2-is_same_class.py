0a46e088ca020b44830bf173ddf8f26c8dbbaacb
matthew25072005 <8855@holbertonstudents.com>
2025-02-06 14:32:03 -0300

    task 02

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a class named MyList.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if obj is exactly an instance of a_class, otherwise False.
+    """
+    return type(obj) is a_class
