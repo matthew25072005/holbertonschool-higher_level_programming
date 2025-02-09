c3f99077e68f5a7a575621cb8e88d621f9a7b5c0
matthew25072005 <8855@holbertonstudents.com>
2025-02-09 00:53:30 -0300

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a module
+"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    """
+    class that inherites Rectangle
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area():
+        return self._size * self.__size
+
