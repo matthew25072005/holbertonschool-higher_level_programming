2f3d96c7017f6fc686c91bf3d9bb6dee98a23833
matthew25072005 <8855@holbertonstudents.com>
2025-02-08 23:46:40 -0300

    task 09

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is a module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    class that inherites basegeometry
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
