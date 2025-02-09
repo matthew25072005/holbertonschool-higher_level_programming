0e1cea289577c56ea917a86bed09c42beecd11da
matthew25072005 <8855@holbertonstudents.com>
2025-02-09 02:41:25 -0300

    task 01

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This is a module
+"""
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """
+    an abstract class named Shape
+    """
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    """
+    concrete class
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14159 * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * 3.14159 * self.radius
+
+class Rectangle(Shape):
+    """
+    concrete class
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
+
