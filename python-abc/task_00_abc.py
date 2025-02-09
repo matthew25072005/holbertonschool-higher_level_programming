409ba2963f6c05427139147ea81226c9d8ec01de
matthew25072005 <8855@holbertonstudents.com>
2025-02-09 01:52:32 -0300

    task 01

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is a module
+"""
+from abc import ABC
+
+
+class Animal(ABC):
+    """
+    an abstract class named Animal
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    """
+    subclasse of Animal
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    subclasse of Animal
+    """
+    def sound(self):
+        return "Meow"
