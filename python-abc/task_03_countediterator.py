1d97e0abd48a5fc3640c36ea6f7a0cc94bef0f21
matthew25072005 <8855@holbertonstudents.com>
2025-02-09 17:57:18 -0300

    task 03

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -3,7 +3,6 @@
 This is a module
 """
 
-
 class CountedIterator:
     """
     A class that wraps an iterator and counts the number of items iterated over.
@@ -14,8 +13,24 @@ class CountedIterator:
         self.count = 0
 
     def __next__(self):
-        self.count += 1
-        return next(self.iterator)
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
 
     def get_count(self):
         return self.count
+
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4]
+    counted_iter = CountedIterator(data)
+
+    try:
+        while True:
+            item = next(counted_iter)
+            print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+    except StopIteration:
+        print("No more items.")
