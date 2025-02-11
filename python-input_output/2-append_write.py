98485e81074d6da8bb3c6d84c97610f0c4162b95
matthew25072005 <8855@holbertonstudents.com>
2025-02-11 11:30:25 -0300

    task 02

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a module
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    a function that appends a string at the end of a text file 
+    """
+    with open(filename, 'a') as f:
+        return f.write(text)
