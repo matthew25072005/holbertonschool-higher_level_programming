1ebff3f0d8927126e63761783cfd31bf2c2bfe38
matthew25072005 <8855@holbertonstudents.com>
2025-02-11 11:31:54 -0300

    task 02

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ This is a module
 
 def append_write(filename="", text=""):
     """
-    a function that appends a string at the end of a text file 
+    a function that appends a string at the end of a text file
     """
     with open(filename, 'a') as f:
         return f.write(text)
