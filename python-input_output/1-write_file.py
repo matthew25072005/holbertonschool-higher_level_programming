309426c420d86717f2221fc61b8f9111b7d4abed
matthew25072005 <8855@holbertonstudents.com>
2025-02-11 11:08:54 -0300

    task 01

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,8 @@ This is a module
 
 
 def write_file(filename="", text=""):
+    """
+    a function that writes a string to a text file (UTF8) and returns the number of characters written
+    """
     with open(filename, 'w') as file:
         return f.write(text)
