70cf6f19a5f3389d3a2d9f842bde0e3c7d732400
matthew25072005 <8855@holbertonstudents.com>
2025-02-11 10:43:46 -0300

    task 00

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     this is a function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
+        print(file.read())
